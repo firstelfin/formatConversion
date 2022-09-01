b9fc9980501ba7dab993567c50cb1794a6bb85e5
industai <firstelfin>
2022-09-01 10:32:48 +0800

    utils init v1

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,3 +3,13 @@
 # @Time    : 2022/8/17 上午11:02
 # @Author  : firstelfin
 # @File    : __init__.py.py
+
+from multi2multi import *
+from multi2single import *
+from video2img import *
+from tool import *
+
+__call__ = [
+    "Yolo2Xml", "Xml2Yolo", "Yolo2Coco", "Video2Img", "rename_files",
+    "split_datasets", "colorstr"
+]
