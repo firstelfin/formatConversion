2ba0d85e730a0cf9e73e338d1ce6c32ad01e6c09
industai <firstelfin>
2022-09-05 10:39:08 +0800

    add write yolov5 txt

--- utils/tool.py
+++ utils/tool.py
@@ -49,16 +49,27 @@ def save_txt(root_dir, split_data, mode="train", year="2017"):
 def split_datasets(path_root, split_ratio, year):
     """
     切分数据集，生成train.txt、valid.txt、test.txt
+
     Args:
-        path_root: 数据集的根目录
-        split_ratio: 切分比例
-        year: 年份
+    ------
+        path_root: 数据集的根目录\n
+        split_ratio: 切分比例\n
+        year: 年份\n
+
+    Examples
+    ------
+        >>> split_datasets(
+        >>>     path_root="/home/industai/sda2/datatsets/smokefire_industai/",
+        >>>     split_ratio=[0.8, 0.1, 0.1],
+        >>>     year=2022
+        >>> )
+
     Returns: 切分后的数据集分布
     """
     all_images_paths = os.listdir(path_root + "/images/")
     train, valid_test = train_test_split(all_images_paths, train_size=split_ratio[0], random_state=2022)
     valid, test = train_test_split(valid_test,
-                                   test_size=split_ratio[-1] / sum(split_ratio[1:]), random_state=2022)
+                                   train_size=split_ratio[1] / sum(split_ratio[1:]), random_state=2022)
     train = add_prefix(train, path_root)
     save_txt(path_root, train, year=year)
     valid = add_prefix(valid, path_root)
@@ -74,6 +85,22 @@ def rename_signal_file(root_path, new_name):
 
 
 def rename_files(root_dir, out_prefix="smokefire_industai", out_suffix=".jpg", out_index=0):
+    """
+    将图片、文件夹下的所有图片重命名
+
+    Args:
+    ------
+        root_dir: 目标根路径\n
+        out_prefix: 输出前缀\n
+        out_suffix: 输出后缀\n
+        out_index: 输出编码\n
+
+    Examples
+    ------
+        >>> rename_files("/home/industai/sda2/datatsets/charging_station/chargingFP/", out_index=23143)
+
+    Returns: next code index.
+    """
     root_path = Path(root_dir)
     if root_path.is_dir():
         for file in root_path.iterdir():
@@ -90,5 +117,45 @@ def rename_files(root_dir, out_prefix="smokefire_industai", out_suffix=".jpg", o
     return out_index
 
 
+def write_yolov5_txt(root_dir, prefix="/home/industai/sda2/datatsets/charging_station",
+                     out="/home/industai/sda2/datatsets/smokefire_industai/valid_charging.txt"):
+    """
+    将root_dir下的所有文件写入yolo训练文件train.txt这种格式
+
+    Args:
+    ------
+        root_dir: 源文件夹\n
+        prefix: 文件前缀\n
+        out: 输出路径
+
+    Examples
+    -------
+        >>> write_yolov5_txt(
+        >>>     root_dir="/home/industai/sda2/datatsets/charging_station/chargingFP/",
+        >>>     prefix="/home/industai/sda2/datatsets/smokefire_industai/images/"
+        >>> )
+
+    Returns:
+    """
+    write_path = Path(root_dir).iterdir()
+    if prefix is None:
+        prefix = root_dir
+    with open(out, "w+", encoding="utf-8") as f:
+        for file in write_path:
+            f.write(prefix + os.sep + file.name + "\n")
+        f.close()
+    pass
+
+
 if __name__ == '__main__':
-    rename_files("/home/industai/sda2/datatsets/charging_images", out_index=21342)
+    # rename_files("/home/industai/sda2/datatsets/charging_station/chargingFP/", out_index=23143)
+    # write_yolov5_txt(
+    #     root_dir="/home/industai/sda2/datatsets/charging_station/chargingFP/",
+    #     prefix="/home/industai/sda2/datatsets/smokefire_industai/images/"
+    # )
+    split_datasets(
+        path_root="",
+        split_ratio=[0.8, 0.1, 0.1],
+        year=2022
+    )
+    pass
